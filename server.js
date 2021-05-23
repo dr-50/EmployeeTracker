f2284939fcf5a6eeeb12c8a3994e1ba4f7a2fc73
Devon <devonross50@yahoo.com>
2021-05-23 09:40:57 -0500

    file structre added

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const inquirer = require('inquirer');
+const cTable = require('console.table');
+
 const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
